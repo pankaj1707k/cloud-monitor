879ba8f1b8cb90047a7a89b6d4a56a81ad0e8ea8
Pankaj Kumar <76695979+pankaj1707k@users.noreply.github.com>
2023-05-06 18:25:17 +0530

    feat: add frontend base code

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import { Routes, Route } from "react-router-dom";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Logs from "./scenes/logs";
+import Events from "./scenes/events";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+
+function App() {
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/logs" element={<Logs />} />
+              <Route path="/events" element={<Events />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
