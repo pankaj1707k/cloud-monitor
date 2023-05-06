0880cf769966ba0633f6c039f50f0078b40fe343
Mitul16 <Mitul16@users.noreply.github.com>
2023-05-06 10:35:45 +0530

    dev: Updated agent configuration

--- attestation_agent/config.py
+++ attestation_agent/config.py
@@ -1,6 +1,17 @@
+import os
+
+# Path to a session file to save the state of the attestation agent
+# so that we can resume last session without re-parsing already parsed logs
+SESSION_FILEPATH = os.path.expanduser("~/.attestation-agent.session")
+
+# Path to various log files
 AUTH_LOG = "/var/log/auth.log"
 AUDIT_LOG = "/var/log/audit/audit.log"
+
+# Path to obtain a device identifier
 MACHINE_ID_PATH = "/etc/machine-id"
-ATTESTATION_HOST = ""
-ATTESTATION_PORT = "8080"
+
+# Attestation server related configurations
+ATTESTATION_HOST = "10.10.10.185"
+ATTESTATION_PORT = "3000"
 BASE_URL = f"http://{ATTESTATION_HOST}:{ATTESTATION_PORT}"
