332f520b076d675250c5bffec2225934fc4707c7
Mitul16 <Mitul16@users.noreply.github.com>
2023-05-06 14:32:39 +0530

    feat: Added Logger class

--- attestation_agent/logs/__init__.py
+++ attestation_agent/logs/__init__.py
@@ -39,6 +39,88 @@ class Event(ABC):
         return "{\n\t" + "\n\t".join(_obj_state) + "}"
 
 
+class Logger(ABC):
+    """
+    Base class for all loggers.
+    """
+
+    def __init__(self) -> None:
+        # Store the state of logger
+        self._running: bool = True
+
+        # Create a mutex lock used to pause the parser to flush generated events
+        self.lock: Lock = Lock()
+
+        # Facilitates faster removal from the front of queue after
+        # log has been analyzed. Removal is required to prevent
+        # high memory consumption when machine runs for longer durations.
+        self.logs: deque[str] = deque()
+
+    def run(self, sio: "socketio.Client") -> None:
+        """
+        Run the logger.
+        """
+        # While the logger can run, it will collect
+        # usage metrics and send them to the attestation server
+        while self._running:
+            # Try to collect usage metrics and catch any error
+            # and print it as LogError
+            try:
+                data = self._collect_log()
+            except Exception as exc:
+                print(
+                    LogError(
+                        msg="error while collecting usage metrics",
+                        exc=exc
+                    ),
+                    file=sys.stderr
+                )
+            finally:
+                # Send the data to the attestation server
+                self._send_log(sio, data)
+
+            # Wait for some time
+            # NOTE: Can be adjusted: SLOW, NORMAL, FAST
+            time.sleep(1)
+
+    @abstractmethod
+    def _collect_log(self) -> dict:
+        """
+        Collect data to be logged
+        For example:
+         - application usage
+         - system usage
+        """
+        raise NotImplementedError("Implementation required for: _collect_log")
+
+    def _send_log(self, sio: "socketio.Client", data: dict) -> None:
+        """
+        Sends data to the attestation server
+        """
+        try:
+            sio.emit("collectLogs", (self.type, data))
+        except Exception as exc:
+            print(
+                LogError(
+                    msg="error while sending logs",
+                    exc=exc
+                ),
+                file=sys.stderr
+            )
+
+    def resume(self):
+        """
+        Set the logger state to running
+        """
+        self._running = True
+
+    def stop(self):
+        """
+        Set the logger state to stopped
+        """
+        self._running = False
+
+
 class Parser(ABC):
     """
     Base class for all parsers. Initialization requires the absolute path
